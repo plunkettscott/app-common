fdfe1dd18cb8e0b6d53c0d5160b2532b571717b7
Scott Plunkett <plunkets@aeoss.io>
2020-01-02 19:01:44 -0600

    feat: remove collect.js

--- packages/extensions-sdk/src/Collection/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Collection } from "collect.js";
-export { collect } from "collect.js";
--- packages/extensions-sdk/src/index.ts
+++ packages/extensions-sdk/src/index.ts
@@ -1,5 +1,3 @@
-export { Collection, collect } from './Collection';
-
 export { default as JSXCollection } from './JSXCollection/JSXCollection';
 export { default as JSXItem } from './JSXCollection/JSXItem';
 export { default as routeUtils } from './module/routes';
